e6487845e2be3501d8a2d0b42a4eeff46652f7ed
johan <gace.johan3@gmail.com>
2020-10-08 11:42:32 -0400

    Template

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,6 +1,26 @@
-export function createPages(){
+import path from 'path'
+// import Pizza from './src/templates/Pizza';
 
 
-    console.log('create pages');
+async function turnPizzasIntoPages({graphql, actions}){
+
+const pizzaTemplate= path.resolve('.src/templates/Pizza.js')
+const {data} = await graphql(`
+	query {
+		pizzas: allSanityPizza {
+			nodes {
+				name 
+				slug {
+					current
+					}
+				}
+			}
+		}
+`)
+console.log(data)
+}
+export async function createPages(params){
+
+await turnPizzasIntoPages(params)
 
 }
---
+++ src/templates/Pizza.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export default function SinglePizzaPage() {
+  return (
+   <div> Pizza</div>
+  );
+}
